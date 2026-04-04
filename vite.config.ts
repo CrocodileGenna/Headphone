7371a39b6a68062c1363c47659b3d9ecaf6abf67
YuriiMoskalenko <99128333+CrocodileGenna@users.noreply.github.com>
2026-04-04 08:20:14 +0300

    gh-p-2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command })=>({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),],
-  base: '/Headphone/',
-});
+  base: command === 'build' ? '/Headphone/' : '/',
+}));
