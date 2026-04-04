e4c3ff3f123ea9a9c3c6554e03df09ffeccf6883
YuriiMoskalenko <99128333+CrocodileGenna@users.noreply.github.com>
2026-04-04 21:25:07 +0300

    gh-p-13

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command })=>({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),],
-  base: command === 'build' ? '/Headphone' : '/',
+  base: command === 'build' ? '/Headphone/' : '/',
    build: {
     outDir: 'build/client'
   }
